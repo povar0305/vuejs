df1bdf1bc29263e349fcbdc0a1359fcdb249f059
SonyaPovar <soficko.sofi@yandex.ru>
2022-08-12 11:27:17 +0300

    двухстороннее связывание через v-model

--- src/components/formAddPost.vue
+++ src/components/formAddPost.vue
@@ -1,8 +1,7 @@
 <template>
   <form v-if="showForm" @submit.prevent>
-    <input v-bind:value='post.titleForm' @input="post.titleForm=$event.target.value" type="text"
-           placeholder="Название поста" required>
-    <input v-bind:value='post.bodyForm' @input="post.bodyForm=$event.target.value" type="text" placeholder="Текст" required>
+    <input v-model='post.titleForm'  type="text"  placeholder="Название поста"  >
+    <input v-model='post.bodyForm'  type="text" placeholder="Текст"  >
     <button @click="sendForm">Добавить пост</button>
   </form>
 </template>
